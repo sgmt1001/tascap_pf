b5cf92421557c5873f07845c605f5cbc56599e69
sgmt1001 <sgmt1001@gmail.com>
2021-08-23 18:36:51 +0000

    reaction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,12 @@
 class Comment < ApplicationRecord
-  
+
   belongs_to :task
   belongs_to :user
-  
+  has_many :reactions, dependent: :destroy
+
+  def reactioned_by?(user)
+    reactions.where(user_id: user.id).exists?
+  end
+
+
 end
