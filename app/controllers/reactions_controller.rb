32be013d2d8e6065ac60acbd73d7ba3a399002e7
sgmt1001 <sgmt1001@gmail.com>
2021-09-20 13:22:23 +0000

    asynchronous

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,6 +4,10 @@ class ReactionsController < ApplicationController
     @comment = Comment.find(params[:comment_id])
     reaction = current_user.reactions.new(comment_id: @comment.id)
     reaction.save
+    @task = @comment.task
+    
+    p @comment
+    p @task
     #redirect_to organization_project_task_path(@comment.task.project.organization_id,@comment.task.project_id,@comment.task_id)
      #非同期化に伴い削除
   end
@@ -12,6 +16,7 @@ class ReactionsController < ApplicationController
     @comment = Comment.find(params[:comment_id])
     reaction = current_user.reactions.find_by(comment_id: @comment.id)
     reaction.destroy
+    @task = @comment.task
     #redirect_to organization_project_task_path(@comment.task.project.organization_id,@comment.task.project_id,@comment.task_id)
     #非同期化に伴い削除
   end
